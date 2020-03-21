83a8ae2e68fab36285e53c6cd7b9ef64ef84698b
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-03-21 18:48:14 +1100

    Added API call and recipe component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = process.env.REACT_APP_APP_ID;
   const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-  console.log(APP_ID);
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = async () => {
+    const link = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const response = await fetch(link);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
